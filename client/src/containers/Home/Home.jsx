fdd4b9eda7127034df30465b2d62ff686c197f14
Jonathan J. Watson <jonathanwatson1@gmail.com>
2021-06-09 15:58:49 -0400

    added basic sections to homepage

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Home.css";
+import ComputerProgrammer from "../../assets/images/computer-programmer.jpg";
+import Planning from "../../assets/images/planning.jpg";
+import JonathanWatson from "../../assets/images/jonathanwatson.jpg";
 
 const Home = () => {
   return (
@@ -9,15 +12,56 @@ const Home = () => {
           What Will You Learn&nbsp;Today?
         </h1>
       </div>
-      <div className="container">
+      <section className="container home-section">
         <div className="row">
-          <div className="col s4">Image will go here.</div>
-          <div className="col s8">
-            <h2>About</h2>
+          <div className="col s12 m4">
+            <img
+              src={ComputerProgrammer}
+              alt="Computer Programmer"
+              className="home-photo"
+            />
+          </div>
+          <div className="col s12 m8">
+            <h2>About Us</h2>
             <p>Something</p>
           </div>
         </div>
-      </div>
+      </section>
+      <section className="container home-section">
+        <div className="row">
+          <div className="col s12 m8">
+            <h2>About Jonathan Watson</h2>
+            <p>
+              Jonathan is a Senior Software Engineer in the Greater Atlanta
+              Area. In addition to extensive experience as a developer he served
+              as Adjunct Instructor at Georgia Tech Professional Education,
+              teaching their Web Development Bootcamp.
+            </p>
+          </div>
+          <div className="col s12 m4">
+            <img
+              src={JonathanWatson}
+              alt="Jonathan Watson Headshot"
+              className="home-photo"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="container home-section">
+        <div className="row">
+          <div className="col s12 m4">
+            <img
+              src={Planning}
+              alt="Planning Next Events"
+              className="home-photo"
+            />
+          </div>
+          <div className="col s12 m8">
+            <h2>Upcoming Events</h2>
+            <p>Something</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
